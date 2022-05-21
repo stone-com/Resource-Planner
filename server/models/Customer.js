4ad8db46cf45eb179f62611fc9f4c7fc50e4b234
Mike Sanders <msanders331@gmail.com>
2022-05-21 11:52:13 -0700

    updated user.js added customer model file.

---
+++ server/models/Customer.js
@@ -0,0 +1,2 @@
+//Customer Model
+const {Schema, model} = require ('mongoose')
--- server/models/User.js
+++ server/models/User.js
@@ -20,6 +20,9 @@ const userSchema = new Schema({
         required: true,
         minlength: 5,
     },
+    customerId:{
+        
+    }
 
 })
 
