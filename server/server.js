6b85602c87b15f6738d55bf5f01899fad8f90abc
Mike Sanders <msanders331@gmail.com>
2022-05-21 19:00:12 -0700

    updated server to include the authmiddleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const {authMiddleware} = require('./utils/auth')
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -11,6 +12,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   csrfPrevention: true,
+  context: authMiddleware,
 
 });
 
