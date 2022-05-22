38e8eaf3c6bcfab8f316c1df446f439bb37b756c
Mike Sanders <msanders331@gmail.com>
2022-05-21 17:55:45 -0700

    updated package js with jwt. Created auth util on server side.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-
-   
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,1 +1,35 @@
 //file where the auth utility functions will be created and called from on the server side.
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretshhhh';
+const expiration = '2h'
+
+module.exports = {
+    authMiddleware: function ({req}){
+        //set the token from the body/query/headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        //if the auth is coming from the header (split the string in to an array, pop the last element which is the toke and trim any whitespace )
+        if(req.headers.authorization){
+            token = token.split(' ').pop().trim();
+        }
+
+        if (!token) {
+            return req;
+        }
+
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+        }
+
+        return req;
+    },
+
+    signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+};
