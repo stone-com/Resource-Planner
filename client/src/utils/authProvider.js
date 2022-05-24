2b4f1a53c07f02b6abf56c99c397486f67d1ca98
Mike Sanders <msanders331@gmail.com>
2022-05-23 22:24:29 -0700

    updated authprovider and app.js

--- client/src/utils/authProvider.js
+++ client/src/utils/authProvider.js
@@ -3,7 +3,26 @@ import {AuthProvider} from 'react-admin';
 
 const authProvider = {
     //Authentication
-    login: params => Promise.resolve()
+    login: ({ username, password }) =>  {
+        const request = new Request('https://mydomain.com/authenticate', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        });
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(auth => {
+                localStorage.setItem('auth', JSON.stringify(auth));
+            })
+            .catch(() => {
+                throw new Error('Network error')
+            });
+        }
 }
 
 export default authProvider
