166fcb83fe2356ebebae57fb86b4c88a8fff378e
Stone Comstock <comstockstone@yahoo.com>
2022-05-27 22:23:01 -0700

    apollo client setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import App from './App';
 
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'https://localhost:3001',
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
