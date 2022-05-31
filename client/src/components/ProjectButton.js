267f8459c6803f70a48eea42ae48afdbeda562f6
CHENG CHIEN <hcc780921@gmail.com>
2022-05-31 16:45:59 -0700

    background added +resource card color changed

--- client/src/components/ProjectButton.js
+++ client/src/components/ProjectButton.js
@@ -80,7 +80,7 @@ export default function ProjectButton() {
 
   return (
     <>
-      <Button className='m-2' variant='success' onClick={handleShow}>
+      <Button id="add-new-project-btn" className='m-2' variant='success' onClick={handleShow}>
         <AiOutlinePlus />
         Add new project
       </Button>
